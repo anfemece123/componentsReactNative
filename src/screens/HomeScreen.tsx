ec3dde95680331b2bc541699e3b9ae7bca62a6ca
anfemece123 <90008499+anfemece123@users.noreply.github.com>
2023-03-16 16:28:08 -0500

    changes android

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Icon name="star-outline" size={30} />
     </View>
   );
 };
